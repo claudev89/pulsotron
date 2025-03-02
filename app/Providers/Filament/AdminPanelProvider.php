d4518503e2e011da1d42ff3c1855482a6bf01097
claudev89 <menaresclaudio08@gmail.com>
2025-03-02 00:34:14 -0300

    fix: paleta de colores establecida

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,7 +28,12 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'danger' => '#73030D',
+                'gray' => '#595048',
+                'info' => '#400207',
+                'primary' => '#324014',
+                'success' => '#324014',
+                'warning' => '#F2D5A0',
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
